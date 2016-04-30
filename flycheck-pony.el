33a299d80737be5d43c090e80346b930cdd5e2cc
Sean T Allen <sean@monkeysnatchbanana.com>
2016-04-30 14:23:30 -0400

    Update copyright info

--- flycheck-pony.el
+++ flycheck-pony.el
@@ -1,36 +1,41 @@
 ;;; flycheck-pony.el --- Pony support in Flycheck
-
-;; Copyright (C) 2016 Richard M. Loveland <r@rmloveland.com>
-
-;; Author: Richard M. Loveland <r@rmloveland.com>
-;; URL: https://github.com/rmloveland/flycheck-pony
+;;
+;; Authors: Sean T Allen <sean@monkeysnatchbanana.com>
+;; Version: 0.1.1
+;; URL: https://github.com/seantallen/flycheck-pony
 ;; Keywords: tools, convenience
-;; Version: 0.1
-
+;; Package-Requires: ((flycheck "0.25.1"))
+;;
 ;; This file is not part of GNU Emacs.
-
+;;
+;; Copyright (C) 2016 Richard M. Loveland <r@rmloveland.com>
+;; Copyright (c) 2016 Sean T. Allen
+;;
+;;; Commentary:
+;;
+;; Pony syntax checking support for Flycheck.  Runs "ponyc -rexpr" in the
+;; current working directory.
+;;
+;; You may need to customize the location of your Pony compiler if
+;; Emacs isn't seeing it on your PATH:
+;;
+;; (setq flycheck-pony-executable "/usr/local/bin/ponyc")
+;;
+;;; License:
+;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
-
-;; Pony syntax checking support for Flycheck.  Runs "ponyc -rexpr" in the
-;; current working directory.
-
-;; You may need to customize the location of your Pony compiler if
-;; Emacs isn't seeing it on your PATH:
-
-;; (setq flycheck-pony-executable "/usr/local/bin/ponyc")
 
 ;;; Code:
 
